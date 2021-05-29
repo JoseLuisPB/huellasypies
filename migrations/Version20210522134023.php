a0be58755e221e4bcfcee6a131654598d951787c
José Luis Peñuela Berenguer <jpenuelaberenguer@gmail.com>
2021-05-29 15:47:58 +0200

    migraciones finales

---
+++ migrations/Version20210522134023.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210522134023 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE mascota CHANGE modificada aprobada TINYINT(1) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE mascota CHANGE aprobada modificada TINYINT(1) NOT NULL');
+    }
+}
